e78d97b5f700866015910361c341eba58b6be1f4
Rabialco Argana <rabialco@gmail.com>
2020-04-09 00:49:44 +0700

    [RED] Try Random Trial Example (Not Dev Stage)

--- src/main/java/com/bot/ribot/RiBotApplication.java
+++ src/main/java/com/bot/ribot/RiBotApplication.java
@@ -1,13 +1,68 @@
 package com.bot.ribot;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
-public class RiBotApplication {
+@LineMessageHandler
+public class RiBotApplication extends SpringBootServletInitializer {
+
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RiBotApplication.class);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(RiBotApplication.class, args);
+        SpringApplication.run(RiBotApplicationgit.class, args);
+    }
+
+    @EventMapping
+    public void handleTextEvent(MessageEvent<TextMessageContent> messageEvent){
+        String pesan = messageEvent.getMessage().getText().toLowerCase();
+        String[] pesanSplit = pesan.split(" ");
+        if(pesanSplit[0].equals("apakah")){
+            String jawaban = getRandomJawaban();
+            String replyToken = messageEvent.getReplyToken();
+            balasChatDenganRandomJawaban(replyToken, jawaban);
+        }
+    }
+
+    private String getRandomJawaban(){
+        String jawaban = "";
+        int random = new Random().nextInt();
+        if(random%2==0){
+            jawaban = "Ya";
+        } else{
+            jawaban = "Nggak";
+        }
+        return jawaban;
+    }
+
+    private void balasChatDenganRandomJawaban(String replyToken, String jawaban){
+        TextMessage jawabanDalamBentukTextMessage = new TextMessage(jawaban);
+        try {
+            lineMessagingClient
+                    .replyMessage(new ReplyMessage(replyToken, jawabanDalamBentukTextMessage))
+                    .get();
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Ada error saat ingin membalas chat");
+        }
     }
 
 }
