09f95d05a1a8db1dfa94017173cf46bf395b5bfc
Rabialco Argana <rabialco@gmail.com>
2020-04-09 00:45:36 +0700

    [GREEN] Initial Commit

---
+++ src/main/java/com/bot/ribot/RiBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.ribot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/ribot/RiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.ribot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
