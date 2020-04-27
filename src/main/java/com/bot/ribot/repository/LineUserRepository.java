2269cba14c6a863b93dfc0b4a588308633854f5b
Salman <muhammadsalman.1505@gmail.com>
2020-04-28 02:29:41 +0700

    Fix database model and repository on it

--- src/main/java/com/bot/ribot/repository/LineUserRepository.java
+++ src/main/java/com/bot/ribot/repository/LineUserRepository.java
@@ -5,13 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface LineUserRepository extends JpaRepository<LineUser, String> {
-    @Query(value = "SELECT * FROM LINE_USER2 WHERE USER_ID = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM LINE_USER WHERE USER_ID = ?1", nativeQuery = true)
     LineUser findLineUserByUserId(String userID);
 
     @Query(value = "SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END "
-           + "FROM LINE_USER2 c WHERE c.USER_ID = ?1", nativeQuery = true)
+           + "FROM LINE_USER c WHERE c.USER_ID = ?1", nativeQuery = true)
     boolean isUserRegistered(String userID);
 
-    @Query(value = "SELECT state FROM LINE_USER2 WHERE USER_ID = ?1", nativeQuery = true)
+    @Query(value = "SELECT state FROM LINE_USER WHERE USER_ID = ?1", nativeQuery = true)
     String findStateById(String userID);
 }
